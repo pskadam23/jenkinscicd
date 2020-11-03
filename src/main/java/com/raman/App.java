933e1e94862b2fd158a08f83813a6b5f9b428e2f
pskadam23 <33250073+pskadam23@users.noreply.github.com>
2020-11-03 10:11:38 +0100

    Update App.java

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello From Git gub" );
+        System.out.println( "Hello From GitHub and Maven" );
     }
 }
