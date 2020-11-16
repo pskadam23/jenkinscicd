a9129cc3faed7778f2b9214dcc2974667a63c767
pskadam23 <33250073+pskadam23@users.noreply.github.com>
2020-11-16 14:52:15 +0100

    Update App.java

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello From Jenkins Git-Job proj" );
+        System.out.println( "Hello From Git hub and maven" );
     }
 }
